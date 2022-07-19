644a702a695487b530573dbe13979121f3edbe87
+ <+>
2022-07-19 14:08:25 -0400

    commit tree 43de41aad57a2254ebea7f408772c2b18fb88edc

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {
+    name: "meep",
+    address: "1000 meep st",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key]:value};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newE= {...employee};
+    delete newE[key];
+    return newE;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
